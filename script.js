d635e241e1fbbfb8287d72cda64280f5129e2dc0
Thain4Souza <168188627+Thain4Souza@users.noreply.github.com>
2025-05-30 11:29:01 -0300

    contador3

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// Variável que guarda o número de cliques
 let contador = 0;
 
-// Função que é chamada quando o botão é clicado
 function incrementar() {
-  contador++; // Soma +1
-  document.getElementById("contador").textContent = contador; // Atualiza o número na tela
+  contador++;
+  document.getElementById("contador").textContent = contador;
 }
 
+function resetar() {
+  contador = 0;
+  document.getElementById("contador").textContent = contador;
+}
